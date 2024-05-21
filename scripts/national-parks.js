761a695eb8c80b17fa4d37b515d896a9364e6c56
scott <scottysora@gmail.com>
2024-05-20 21:49:30 -0400

    create select option functions

--- scripts/national-parks.js
+++ scripts/national-parks.js
@@ -1,6 +1,20 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const parkResultsContainer = document.getElementById("results");
+  const locationSelect = document.getElementById("location");
+  const parkTypeSelect = document.getElementById("type");
+  const searchInput = document.getElementById("search");
 
-function loadParks() {
-  for (const park of nationalParksArray) {
+  // Create options for select elements
+  for (const location of locationsArray) {
+    const locationOption = document.createElement("option");
+    locationOption.value = location;
+    locationOption.text = location;
+    locationSelect.appendChild(locationOption);
   }
-}
 
+  for (const parkType of parkTypesArray) {
+    const parkTypeOption = document.createElement("option");
+    parkTypeOption.value = parkType;
+    parkTypeOption.text = parkType;
+    parkTypeSelect.appendChild(parkTypeOption);
+  }
