e1f0c833723e35d24550aa098f98389ee013b906
scott <scottysora@gmail.com>
2024-05-21 20:23:23 -0400

    create mountain card and filter to select mountains

--- scripts/mountains.js
+++ scripts/mountains.js
@@ -13,3 +13,25 @@ function createTextElement(className, text) {
   return element;
 }
 
+function createMountainCard(item) {
+  const card = document.createElement("div");
+  card.classList.add("card");
+
+  card.appendChild(createImageElement(item.img));
+  card.appendChild(createTextElement("card-title", item.name));
+  card.appendChild(createTextElement("card-description", `${item.desc}`));
+
+  return card;
+}
+
+function filter() {
+  const selectedname = document.getElementById("name").value;
+  if (selectedname === "Select a Mountain") {
+    showMountains(mountainsArray);
+  } else {
+    showMountains(mountainsArray.filter(function(mountain) {
+      return mountain.name === selectedname;
+    }));
+  }
+}
+
