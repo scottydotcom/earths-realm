92ce65c5c62ed94db538c1e94b344b82b6dafa7d
scott <scottysora@gmail.com>
2024-05-22 15:32:13 -0400

    add functino to load and get park attributes

--- scripts/national-parks.js
+++ scripts/national-parks.js
@@ -1,6 +1,25 @@
 function option(text) {
   const parkOption = document.createElement("option");
-  parkOption.innerText = text;
+  parkOption.innerHTML = text;
   return parkOption;
 }
 
+function getPark(park) {
+  const element = document.createElement("div");
+  element.classList.add("park");
+
+  element.innerHTML = `
+        <hr>
+        <h3>${park.LocationName}</h3>
+        <h4>${park.LocationID}</h4>
+        <div>${park.State}</div>
+    `;
+  if (park.hasOwnProperty("Visit")) {
+    const link = park.Visit;
+    const text = park.LocationName;
+    element.innerHTML += `
+        <div><a href="${link}" class="visit-site"> ${text} </a></div>
+        `;
+  }
+  return element;
+}
